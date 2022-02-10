8c6acdbf4c293fa2506bc6620dd5cdbd0f4f4fbc
Anees Ahmad <inbox.a.ahmad@gmail.com>
2022-02-10 16:29:57 +0100

    getting email from contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,39 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Input, Form, Row, TextArea, InfoSec,
 } from './ContactElements'
 import GenIcon from './GenIcon'
 import { Text, Button } from '../common';
+import { send } from 'emailjs-com';
+
+
 
 const Contact = () => {
+
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+    });
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_h5q3rus',
+            'template_r4zlraj',
+            toSend,
+            'user_ZQgtvcIGS6YR9MDDF6Un8'
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+            });
+    };
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
+
     return <>
         <Row>
             <InfoSec>
                 <Text color='white' size='24px'>Contact Info</Text>
                 <GenIcon />
             </InfoSec>
-            <Form>
+            <Form onSubmit={onSubmit}>
                 <Input
-                    // value={''}
-                    onChange={''}
-                    name="Name"
-                    type="text"
-                    placeholder="Name" />
+                    type='text'
+                    name='from_name'
+                    placeholder='from name'
+                    value={toSend.from_name}
+                    onChange={handleChange} />
                 <Input
-                    //value={''}
-                    onChange={''}
-                    name="username"
-                    type="email"
-                    placeholder="Email" />
+                    type='text'
+                    name='reply_to'
+                    placeholder='Your email'
+                    value={toSend.reply_to}
+                    onChange={handleChange} />
                 <Input
-                    // value={''}
-                    onChange={''}
-                    name='mobil'
-                    type="number"
-                    placeholder="Mobile" />
+                    type='text'
+                    name='to_name'
+                    placeholder='to name'
+                    value={toSend.to_name}
+                    onChange={handleChange} />
                 <TextArea
+                    type='text'
+                    name='message'
+                    placeholder='Your message'
+                    value={toSend.message}
+                    onChange={handleChange}
                 />
-                <Button fontBig>Submit</Button>
+                <Button fontBig type='submit'>Submit</Button>
             </Form>
         </Row>
     </>;
